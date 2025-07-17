d2c08aebfa9e86340364dec38ebb980e0db41f70
ayushpathak-48 <ayush010pathak@gmail.com>
2025-07-17 18:15:32 +0530

    Add project files.

---
+++ WebApplication4/FruitsWebsite.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WebApplication4
+{
+    public partial class FruitsWebsite : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblMessage.Text = "Select a fruit to view its image.";
+            }
+        }
+
+        protected void lstFruits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedImage = lstFruits.SelectedValue;
+            imgFruit.ImageUrl = "~/Images/" + selectedImage;
+            lblMessage.Text = "You selected: " + lstFruits.SelectedItem.Text;
+        }
+
+    }
+}
